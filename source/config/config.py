465a696e6285f906733cc855215120252e6896fb
kehiy <kehiiiiya@gmail.com>
2024-09-19 23:46:59 +0330

    feat(config): loading.

--- source/config/config.py
+++ source/config/config.py
@@ -1,3 +1,35 @@
-# find the configs document on documents/config.md.
+from __future__ import annotations
 
 import json
+from typing import Dict, List, Union
+
+
+class Config:
+    def __init__(self, path: str) -> None:
+        self.path = path
+        self.name: str = ""
+        self.target: str = ""
+        self.tests: Dict[str, ProtocolModel] = {}
+
+    def load(self) -> None:
+        with open(self.path) as f:
+            jr = json.load(f)
+            self.name = jr["name"]
+            self.target = jr["target"]
+            self.tests = jr["tests"]
+
+    def to_dict(self) -> Dict[str, Union[str, Dict[str, ProtocolModel]]]:
+        return {"name": self.name, "target": self.target, "tests": self.tests}
+
+    def __str__(self) -> str:
+        return json.dumps(self.to_dict(), indent=4)
+
+
+class ProtocolModel:
+    def __init__(self, outdir: str, cases: List[str], excluded_cases: List[str]) -> None:
+        self.outdir = outdir
+        self.cases = cases
+        self.excluded_cases = excluded_cases
+
+    def to_dict(self) -> Dict[str, Union[str, List[str]]]:
+        return {"outdir": self.outdir, "cases": self.cases, "excluded_cases": self.excluded_cases}
--- source/main.py
+++ source/main.py
@@ -1,1 +1,20 @@
+import argparse
+from config.config import Config
 
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="znrt is nostr relay testsiute.")
+    parser.add_argument("config_file", type=str, help="path to the json configuration file.")
+    args = parser.parse_args()
+
+    cfg = Config(args.config_file)
+    try:
+        cfg.load()
+        print("configuration loaded successfully:")
+        print(cfg)
+    except Exception as e:
+        print(f"failed to load configuration: {e}")
+
+
+if __name__ == "__main__":
+    main()
